f60e0cea5eb4d10810a327c94e9628bd43eb631c
Kalpesh Ahire <69343990+Kalpesh-18@users.noreply.github.com>
2022-11-11 15:24:39 +0530

    Update add.c

--- add.c
+++ add.c
@@ -4,6 +4,6 @@ int main(){
 	int firstVariable = 8;
 	int secondVariable = 10;
 	int sum = firstVariable + secondVariable;
-	printf("The sum of %d and %d is %d", a, b, sum);
+	printf("The sum of %d and %d is %d\n", firstVariable, secondVariable, sum);
 	return 0;
 }
