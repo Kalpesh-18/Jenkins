0a5dd93160f3a8ac91857110d32ae89294df0b55
Kalpesh Ahire <69343990+Kalpesh-18@users.noreply.github.com>
2022-11-11 15:23:07 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+//Adding some comments to the program
 int main(){
-	int a = 8;
-	int b = 10;
-	int sum = a + b;
+	int firstVariable = 8;
+	int secondVariable = 10;
+	int sum = firstVariable + secondVariable;
 	printf("The sum of %d and %d is %d", a, b, sum);
 	return 0;
 }
